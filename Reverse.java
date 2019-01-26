483c50c86349c6dc012cc3acfccbf2bf9c80a31d
Kavya Kesavan <kavyakk7@users.noreply.github.com>
2019-01-25 18:02:44 -0600

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,14 +1,15 @@
 class Reverse {
     public int reverse(int x) {
-        boolean isNegative = x<0?true:false;
-        if(isNegative)
-            x = x * -1;
-        int reverse = 0; 
-        while(x > 0)
-        { 
-            reverse = (reverse * 10) + (x % 10); 
-            x = x / 10; 
+        int result = 0;
+        while (x != 0)
+        {
+            int rem = x % 10;
+            int newResult = result * 10 + rem;
+            if ((newResult - rem) / 10 != result)
+            { return 0; }
+            result = newResult;
+            x = x / 10;
         }
-        return isNegative == true? reverse*-1 : reverse;
+        return result;
     }
 }
